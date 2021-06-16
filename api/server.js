232be2e0545c6a4cb9e788760f18b2600e475c9e
unknown <dylanpage212@gmail.com>
2021-06-16 13:53:33 -0700

    finish project

--- api/server.js
+++ api/server.js
@@ -1,35 +1,57 @@
-const express = require("express");
-const helmet = require("helmet");
-const cors = require("cors");
-
-/**
-  Do what needs to be done to support sessions with the `express-session` package!
-  To respect users' privacy, do NOT send them a cookie unless they log in.
-  This is achieved by setting 'saveUninitialized' to false, and by not
-  changing the `req.session` object unless the user authenticates.
-
-  Users that do authenticate should have a session persisted on the server,
-  and a cookie set on the client. The name of the cookie should be "chocolatechip".
-
-  The session can be persisted in memory (would not be adecuate for production)
-  or you can use a session store like `connect-session-knex`.
- */
+const path = require('path'); 
+const express = require('express');
+const helmet = require('helmet'); 
+const cors = require('cors');
+const session = require('express-session');
+const Store = require('connect-session-knex')(session);
+const knex = require('../data/db-config');
 
 const server = express();
+const authRouter = require('./auth/auth-router');
+const userRouter = require('./users/users-router');
+const { NONAME } = require('dns'); //eslint-disable-line
 
-server.use(helmet());
+server.use(express.static(path.join(__dirname, '../client'))); // whats all this about?
+
+server.use(
+	session({
+		name: 'chocolatechip',
+		secret: 'keep it secret', // .env file
+		saveUninitialized: false, // session not saved automatically
+		resave: false, // required by some session stores
+		store: new Store({
+			knex,
+			createTable: true,
+			clearInterval: 1000 * 60 * 10,
+			tablename: 'sessions',
+			sidfieldname: 'sid' // whats all this about?
+		}),
+		cookie: {
+			maxAge: 1000 * 60 * 10,
+			secure: false, // if true, only works over TLS/https
+			httpOnly: false // if true, cookie not in document
+			// sameSite: 'none',
+		}
+	})
+);
+
+server.use(helmet()); // whats all this about?
 server.use(express.json());
 server.use(cors());
 
-server.get("/", (req, res) => {
-  res.json({ api: "up" });
+server.use('/api/users', userRouter);
+server.use('/api/auth', authRouter);
+
+server.get('/', (req, res) => {
+	res.json({ api: 'up' });
 });
 
-server.use((err, req, res, next) => { // eslint-disable-line
-  res.status(err.status || 500).json({
-    message: err.message,
-    stack: err.stack,
-  });
+server.use((err, req, res, next) => { //eslint-disable-line
+	// eslint-disable-line
+	res.status(err.status || 500).json({
+		message: err.message,
+		stack: err.stack
+	});
 });
 
 module.exports = server;
